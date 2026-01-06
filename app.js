9345d644b925aa413fab133c4757c3ef5709ba4c
adil-khan-723 <adilk81054@gmail.com>
2026-01-07 00:17:51 +0530

    Add comment for server listening port
    Added a comment to indicate the server's listening port.

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get("/", async (req, res) => {
   const hash = await bcrypt.hash("docker", 10);
   res.send(`Hello from ${os.platform()}! Hash: ${hash.substring(0, 10)}...`);
 });
-
+//Listens on port 3000
 app.listen(3000, () => console.log("Running on port 3000"));
